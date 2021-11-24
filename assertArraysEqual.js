ffc7a563d870b12045509e83600066ab7b3b4c2e
Amir Sedaghat <amir.sedaghat@gmail.comgit config --global user.email amir.sedaghat@gmail.com>
2021-11-24 01:08:36 +0000

    change assertEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+assertArraysEqual([2,1], [1, 2]);
