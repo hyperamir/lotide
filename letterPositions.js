b8938a8c9b06f1571b847506894f09b808167a2c
Amir Sedaghat <amir.sedaghat@gmail.comgit config --global user.email amir.sedaghat@gmail.com>
2021-11-24 23:38:26 +0000

    position of letters in an array

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+const test = 'lighthouse in the house';
+letterPositions(test);
+console.log(letterPositions(test));
+assertArraysEqual(letterPositions("hello")['e'], [1]);
