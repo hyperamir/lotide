5bc44d3692ea726ab98d1f9c25c21ef03fb2907f
Amir Sedaghat <amir.sedaghat@gmail.comgit config --global user.email amir.sedaghat@gmail.com>
2021-11-29 21:16:56 +0000

    adding chai and moch for tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,18 @@
+const assert = require('chai').assert;
+
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual')
 
-console.log(assertArraysEqual(middle([]), ([])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3, 4])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4])));
+describe('#middle', () => {
+  it('returns [3,4] for [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]));
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(assertArraysEqual((middle([])), []));
+  });
+});
+
+
+// console.log(assertArraysEqual(middle([]), ([])));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3, 4])));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4])));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const {tail} = require('../tail');
-const {assertEqual} = require('../tail');
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+const { tail } = require('../tail');
+const { assertEqual } = require('../tail');
+
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
